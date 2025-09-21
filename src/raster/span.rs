367a19f2616c690b970aa397cf238981567695ba
DGriffin91 <github@dgdigital.net>
2025-09-20 21:31:22 -0700

    split out calc_row_span

--- src/raster/span.rs
+++ src/raster/span.rs
@@ -1,7 +1,6 @@
-use crate::raster::bary::stepper_from_ss_tri_backface_cull;
+use crate::raster::bary::{SingleStepper, stepper_from_ss_tri_backface_cull};
 
 /// ss for screen space (unit is screen pixel)
-/// sp for subpixel space (unit fraction of screen pixel)
 pub fn raster_tri_span<const SUBPIX_BITS: i32>(
     ss_bounds: [i32; 4],
     ss_tri: &[egui::Vec2; 3],
@@ -18,63 +17,62 @@ pub fn raster_tri_span<const SUBPIX_BITS: i32>(
 
     for ss_y in ss_min.y..=ss_max.y {
         stepper.row_start();
-        let w0 = stepper.w0 + stepper.bias0;
-        let w1 = stepper.w1 + stepper.bias1;
-        let w2 = stepper.w2 + stepper.bias2;
 
-        let sx0 = stepper.e12.step.x;
-        let sx1 = stepper.e20.step.x;
-        let sx2 = stepper.e01.step.x;
+        if let Some((start, end)) = calc_row_span(&stepper, max_cols) {
+            let ss_start = ss_min.x + start;
+            let ss_end = ss_min.x + end;
+            span(ss_start, ss_end, ss_y);
+        };
 
-        let mut start = 0;
-        let mut end = i64::MAX;
-
-        if w0 < 0 {
-            if sx0 > 0 {
-                start = start.max(div_ceil(-w0, sx0));
-            } else {
-                stepper.row_step();
-                continue;
-            }
-        } else if sx0 < 0 {
-            end = end.min(div_floor(w0, -sx0) + 1);
-        }
-
-        if w1 < 0 {
-            if sx1 > 0 {
-                start = start.max(div_ceil(-w1, sx1));
-            } else {
-                stepper.row_step();
-                continue;
-            }
-        } else if sx1 < 0 {
-            end = end.min(div_floor(w1, -sx1) + 1);
-        }
-
-        if w2 < 0 {
-            if sx2 > 0 {
-                start = start.max(div_ceil(-w2, sx2));
-            } else {
-                stepper.row_step();
-                continue;
-            }
-        } else if sx2 < 0 {
-            end = end.min(div_floor(w2, -sx2) + 1);
-        }
-
-        end = end.clamp(0, max_cols);
-
-        if start >= end {
-            stepper.row_step();
-            continue;
-        }
-
-        let ss_start = ss_min.x + start;
-        let ss_end = ss_min.x + end;
-
-        span(ss_start, ss_end, ss_y);
         stepper.row_step();
     }
+}
+
+/// Returns Some((start, end)) for the current row in the triangle. The end points are defined within the aabb of the
+/// triangle so add ss_min.x to each to get the screen space coordinate. Returns None if there is no span intersecting
+/// this row.
+fn calc_row_span(stepper: &SingleStepper, max_cols: i64) -> Option<(i64, i64)> {
+    let mut start = 0;
+    let mut end = i64::MAX;
+    let w0 = stepper.w0 + stepper.bias0;
+    let w1 = stepper.w1 + stepper.bias1;
+    let w2 = stepper.w2 + stepper.bias2;
+    let sx0 = stepper.e12.step.x;
+    let sx1 = stepper.e20.step.x;
+    let sx2 = stepper.e01.step.x;
+    if w0 < 0 {
+        if sx0 > 0 {
+            start = start.max(div_ceil(-w0, sx0));
+        } else {
+            return None;
+        }
+    } else if sx0 < 0 {
+        end = end.min(div_floor(w0, -sx0) + 1);
+    }
+    if w1 < 0 {
+        if sx1 > 0 {
+            start = start.max(div_ceil(-w1, sx1));
+        } else {
+            return None;
+        }
+    } else if sx1 < 0 {
+        end = end.min(div_floor(w1, -sx1) + 1);
+    }
+    if w2 < 0 {
+        if sx2 > 0 {
+            start = start.max(div_ceil(-w2, sx2));
+        } else {
+            return None;
+        }
+    } else if sx2 < 0 {
+        end = end.min(div_floor(w2, -sx2) + 1);
+    }
+    end = end.clamp(0, max_cols);
+
+    if start >= end {
+        return None;
+    }
+    Some((start, end))
 }
 
 // --------------------------------------------------------------------------------------------
