badaa9369675cefc8311bf047690815bc6fe2308
DGriffin91 <github@dgdigital.net>
2025-09-23 02:10:09 -0700

    improve naming clarity

--- src/raster/tri.rs
+++ src/raster/tri.rs
@@ -29,13 +29,13 @@ pub fn draw_tri<const SUBPIX_BITS: i32>(
 
     let step_rcp = step_rcp(&stepper);
 
-    let mut c_stepper = if vert_col_vary {
+    let mut vert_col_stepper = if vert_col_vary {
         stepper.attr(&draw.colors, sp_inv_area)
     } else {
         Default::default()
     };
 
-    let mut uv_stepper = if vert_uvs_vary {
+    let mut vert_uv_stepper = if vert_uvs_vary {
         stepper.attr(&draw.uv, sp_inv_area)
     } else {
         Default::default()
@@ -46,18 +46,18 @@ pub fn draw_tri<const SUBPIX_BITS: i32>(
     for ss_y in ss_min.y..=ss_max.y {
         stepper.row_start();
         if vert_col_vary {
-            c_stepper.row_start();
+            vert_col_stepper.row_start();
         }
         if vert_uvs_vary {
-            uv_stepper.row_start();
+            vert_uv_stepper.row_start();
         }
 
         if let Some((start, end)) = calc_row_span(&stepper, max_cols, &step_rcp) {
             if vert_col_vary {
-                c_stepper.attr += c_stepper.step_x * start as f32;
+                vert_col_stepper.attr += vert_col_stepper.step_x * start as f32;
             }
             if vert_uvs_vary {
-                uv_stepper.attr += uv_stepper.step_x * start as f32;
+                vert_uv_stepper.attr += vert_uv_stepper.step_x * start as f32;
             }
             let ss_start = (ss_min.x + start) as usize;
             let ss_end = (ss_min.x + end) as usize;
@@ -73,8 +73,8 @@ pub fn draw_tri<const SUBPIX_BITS: i32>(
                         egui_blend_u8_slice_one_src_tinted_fn_sse41(
                             draw.const_tex_color_u8x4,
                             || {
-                                let v = vec4_to_u8x4_no_clamp(&c_stepper.attr);
-                                c_stepper.col_step();
+                                let v = vec4_to_u8x4_no_clamp(&vert_col_stepper.attr);
+                                vert_col_stepper.col_step();
                                 v
                             },
                             dst,
@@ -87,12 +87,12 @@ pub fn draw_tri<const SUBPIX_BITS: i32>(
                 for ss_x in ss_start..ss_end {
                     let src = if vert_uvs_vary || vert_col_vary {
                         let tex_color = if vert_uvs_vary {
-                            texture.sample_bilinear(uv_stepper.attr)
+                            texture.sample_bilinear(vert_uv_stepper.attr)
                         } else {
                             draw.const_tex_color_u8x4
                         };
                         let vert_color = if vert_col_vary {
-                            vec4_to_u8x4_no_clamp(&c_stepper.attr)
+                            vec4_to_u8x4_no_clamp(&vert_col_stepper.attr)
                         } else {
                             draw.const_vert_color_u8x4
                         };
@@ -107,10 +107,10 @@ pub fn draw_tri<const SUBPIX_BITS: i32>(
                         src
                     };
                     if vert_col_vary {
-                        c_stepper.col_step();
+                        vert_col_stepper.col_step();
                     }
                     if vert_uvs_vary {
-                        uv_stepper.col_step();
+                        vert_uv_stepper.col_step();
                     }
                 }
             }
@@ -118,10 +118,10 @@ pub fn draw_tri<const SUBPIX_BITS: i32>(
 
         stepper.row_step();
         if vert_col_vary {
-            c_stepper.row_step();
+            vert_col_stepper.row_step();
         }
         if vert_uvs_vary {
-            uv_stepper.row_step();
+            vert_uv_stepper.row_step();
         }
     }
 }
